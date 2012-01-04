7e1afb21e951bbe279ff822c393a22906c665a2c
Mateusz Zajac <deejay.zajac@gmail.com>
2012-01-04 13:49:08 +0100

    Added another method of backuping

--- skrypt.sh
+++ skrypt.sh
@@ -1,18 +1,36 @@
 #!/bin/sh
 
-echo "Wybierz folder/plik którego kopiê chcesz wykonaæ"
+echo "Choose file/folder that you want to backup"
 read file
-echo "Wybierz opcjê kopii [1]lokalnie/[2]zewnêtrznie"
+echo "Do backup [1]Local or [2]Remote"
 read letter
+echo "Choose method of backup"
+echo "[1].ZIP [2].TAR.GZ"
+read method
 NOWDATE=`date +%d%m%y`
+
+if [ $method -eq 2 ]; then
 tar -cvzf /usr/backup/$NOWDATE.tar.gz $file
+fi
+if [ $method -eq 1 ]; then
+zip -R /usr/backup/$NOWDATE.zip $file
+fi
 echo Packing complete
 if [ $letter -eq 2 ]; then
+if [ $method -eq 1 ]; then
+ftp -n -i infug.net78.net <<EOF
+user a4860405 haslo6
+put /usr/backup/$NOWDATE.zip $NOWDATE.zip
+!
+EOF
+fi
+if [ $method -eq 2 ]; then
 ftp -n -i infug.net78.net <<EOF
 user a4860405 haslo6
 put /usr/backup/$NOWDATE.tar.gz $NOWDATE.tar.gz
 !
 EOF
+fi
 fi
 if [ $letter -eq 1 ]; then
 echo "Choose your backup folder"
